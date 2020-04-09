fa62c32c043772215f56dcbff664c38ed94c1534
Johan Griesel <johan@stratusolve.com>
2020-04-09 09:43:23 +0200

    Clean up

--- template/divblox_react_native.js
+++ template/divblox_react_native.js
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) 2020. Stratusolve (Pty) Ltd, South Africa
- * This file is the property of Stratusolve (Pty) Ltd.
- * This file may not be used or included in any project prior to the signing of the divblox software license agreement.
- * By using this file or including it in your project you agree to the terms and conditions stipulated by the divblox software license agreement.
- * This file may not be copied or modified in any way without prior written permission from Stratusolve (Pty) Ltd
- * THIS FILE SHOULD NOT BE EDITED. divblox assumes the integrity of this file. If you edit this file, it could be overridden by a future divblox update
- * For queries please send an email to support@divblox.com
- */
 import React, {Component} from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
 import {server_base_url as server_url} from './app.json';
