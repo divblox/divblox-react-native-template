99b39e44e024ab05ef78a6a96041ff666e8be09d
Johan Griesel <johan@stratusolve.com>
2021-07-01 16:44:27 +0200

    Working on splash screens and icons

--- new_template/App.js
+++ new_template/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import RNBootSplash from "react-native-bootsplash";
 
 import DivbloxReactNative from "./divblox/divblox";
 DivbloxReactNative.doInit();
 const NavigationStack = DivbloxReactNative.getNavigationStack();
 
 const App = () => {
+    React.useEffect(() => {
+        const init = async () => {
+            // …do multiple sync or async tasks
+        };
+    
+        init().finally(async () => {
+            await RNBootSplash.hide({ fade: true });
+        });
+    },[]);
     return (
         <NavigationStack/>
     );
--- new_template/divblox/divblox.js
+++ new_template/divblox/divblox.js
@@ -27,6 +27,9 @@ import {
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import SafeAreaView from 'react-native-safe-area-view';
 import {WebView} from 'react-native-webview';
+import NetInfo from '@react-native-community/netinfo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import DeviceInfo from 'react-native-device-info';
 
 const dimensions = Dimensions.get('window');
 
@@ -34,12 +37,17 @@ const DivbloxReactNative = {
   dxAuthenticationToken: null,
   pushRegistrationId: null,
   navigationStack: null,
+  serverFinalUrl: serverBaseUrl + '/?view=native_landing&init_native=1',
+  isConnected: false,
+  currentNavigationObject: null,
+  currentActiveScreen: null,
   doInit() {
     if (isDivbloxWebApp) {
       this.initDivbloxWeb();
     } else {
       this.initDivbloxPureNative();
     }
+    this.registerEventHandlers();
   },
   initDivbloxWeb() {
     this.navigationStack = DivbloxWebStack;
@@ -47,8 +55,36 @@ const DivbloxReactNative = {
   initDivbloxPureNative() {
     this.navigationStack = DivbloxPureNativeStack;
   },
+  registerEventHandlers() {
+    NetInfo.addEventListener(state => {
+      if (this.currentNavigationObject !== null) {
+        if (!state.isConnected && this.isConnected) {
+          this.currentNavigationObject.navigate('Offline');
+        } else if (state.isConnected && !this.isConnected) {
+          this.currentNavigationObject.navigate(this.currentActiveScreen);
+        }
+      }
+
+      this.isConnected = state.isConnected;
+    });
+  },
+  setNavigation(navigation, screen) {
+    this.currentNavigationObject = navigation;
+    this.currentActiveScreen = screen;
+    console.log('Navigation set: ' + this.currentActiveScreen);
+  },
   getNavigationStack() {
     return this.navigationStack;
+  },
+  getWebWrapperUrl() {
+    return this.serverFinalUrl;
+  },
+  loadAppEntryPointScreen() {
+    if (isDivbloxWebApp) {
+      this.currentNavigationObject.navigate('WebWrapper');
+    } else {
+      this.currentNavigationObject.navigate('Placeholder');
+    }
   },
   /**
    * Determines whether a string is a valid JSON string
@@ -89,7 +125,8 @@ const DivbloxReactNative = {
    */
   receiveMessageFromWeb(event) {
     let receivedData = this.getJsonObject(event.nativeEvent.data);
-    switch (receivedData.functionToExecute) {
+    console.log('Received payload: ' + JSON.stringify(receivedData));
+    switch (receivedData.function_to_execute) {
       case 'redirectToExternalPath':
         Linking.canOpenURL(receivedData.redirect_url).then(supported => {
           if (supported) {
@@ -117,8 +154,110 @@ const DivbloxReactNative = {
         });
         break;
       default:
-        console.log('Function not implemented');
+        console.log(
+          'Function not implemented. Received payload: ' +
+            JSON.stringify(receivedData),
+        );
     }
+  },
+  async dxRequestInternal(url, postBodyObj) {
+    try {
+      let response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postBodyObj),
+      });
+      let json = await response.json();
+      if (typeof json.Result !== 'undefined') {
+        if (json.Result === 'Success') {
+          return json;
+        } else {
+          return {dxRequestInternalError: json};
+        }
+      } else {
+        return {dxRequestInternalError: json};
+      }
+    } catch (error) {
+      console.error(error);
+      return {dxRequestInternalError: error};
+    }
+  },
+  async registerDevice() {
+    try {
+      this.dxAuthenticationToken = await AsyncStorage.getItem(
+        'dxAuthenticationToken',
+      );
+      const registerResult = await this.dxRequestInternal(
+        serverBaseUrl + '/api/client_authentication_token/registerDevice',
+        {
+          AuthenticationToken: this.dxAuthenticationToken,
+          DeviceUuid: DeviceInfo.getUniqueId(),
+          DevicePlatform: DeviceInfo.getDeviceId(),
+          DeviceOs: DeviceInfo.getSystemName(),
+        },
+      );
+      console.log('Register result: ' + JSON.stringify(registerResult));
+      if (typeof registerResult.dxRequestInternalError === 'undefined') {
+        if (registerResult.Result === 'Success') {
+          this.dxAuthenticationToken =
+            registerResult.DeviceLinkedAuthenticationToken;
+          await AsyncStorage.setItem(
+            'dxAuthenticationToken',
+            registerResult.DeviceLinkedAuthenticationToken,
+          );
+          if (this.serverFinalUrl.indexOf('auth_token=') < 1) {
+            this.serverFinalUrl += '&auth_token=' + this.dxAuthenticationToken;
+            console.log('Final url set: ' + this.serverFinalUrl);
+            this.loadAppEntryPointScreen();
+          }
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async registerPushNotifications() {
+    console.log(
+      'TODO: Put your code that asks for push notification permissions here. Once a successful Push' +
+        ' registration ID is received, send it to the server with',
+    );
+    //this.createPushRegistration([The ID received from the push service]);
+  },
+  async createPushRegistration(registrationId) {
+    let returnData = {error: 'Unknown'};
+    if (typeof registrationId === 'undefined') {
+      returnData.error = 'No registration id provided';
+      return returnData;
+    }
+    this.pushRegistrationId = await AsyncStorage.getItem('PushRegistrationId');
+    if (this.pushRegistrationId !== null) {
+      // We only want to send the push registration once
+      returnData.error = 'Already registered';
+      return returnData;
+    }
+
+    const pushRegistrationResult = await this.dxRequestInternal(
+      serverBaseUrl + '/api/global_functions/updatePushRegistration',
+      {
+        registration_id: registrationId,
+        device_uuid: DeviceInfo.getUniqueId(),
+        device_platform: DeviceInfo.getDeviceId(),
+        device_os: DeviceInfo.getSystemName(),
+        AuthenticationToken: this.dxAuthenticationToken,
+      },
+    );
+    if (typeof pushRegistrationResult.dxRequestInternalError === 'undefined') {
+      if (pushRegistrationResult.Result === 'Success') {
+        await AsyncStorage.setItem('PushRegistrationId', registrationId);
+      }
+    }
+  },
+  async reInitDevice() {
+    await AsyncStorage.removeItem('dxAuthenticationToken');
+    await this.registerDevice();
   },
 };
 
@@ -165,14 +304,18 @@ const Stack = createStackNavigator();
 
 //#region Global Screens
 const DivbloxGlobalInitScreen = ({navigation}) => {
-  React.useEffect(
-    () =>
-      setTimeout(function () {
-        //TODO: This must decide whether to go to welcome or to load the first screen
+  React.useEffect(() => {
+    DivbloxReactNative.setNavigation(navigation, 'Init');
+    (async () => {
+      const isFirstLaunch = await AsyncStorage.getItem('isFirstLaunch');
+      await DivbloxReactNative.registerDevice();
+      if (isFirstLaunch === null) {
         navigation.navigate('Welcome');
-      }, 1000),
-    [navigation],
-  );
+      } else {
+        DivbloxReactNative.loadAppEntryPointScreen();
+      }
+    })();
+  }, [navigation]);
   return (
     <>
       <SafeAreaView style={styles.fullscreenContainer}>
@@ -183,6 +326,9 @@ const DivbloxGlobalInitScreen = ({navigation}) => {
   );
 };
 const DivbloxGlobalWelcomeScreen = ({navigation, route}) => {
+  React.useEffect(() => {
+    DivbloxReactNative.setNavigation(navigation, 'Welcome');
+  }, [navigation]);
   return (
     <>
       <SafeAreaView style={styles.fullscreenContainer}>
@@ -190,7 +336,7 @@ const DivbloxGlobalWelcomeScreen = ({navigation, route}) => {
         <Text style={{color: fullscreenTextColor}}>Welcome</Text>
         <Button
           title="Go to app"
-          onPress={() => navigation.navigate('WebWrapper')}
+          onPress={() => DivbloxReactNative.loadAppEntryPointScreen()}
           color={fullscreenTextColor}
         />
       </SafeAreaView>
@@ -201,6 +347,9 @@ const DivbloxGlobalOfflineScreen = ({navigation, route}) => {
   return <Text>Offline screen to be built</Text>;
 };
 const DivbloxGlobalErrorScreen = ({navigation, route}) => {
+  React.useEffect(() => {
+    DivbloxReactNative.setNavigation(navigation, 'Error');
+  }, [navigation]);
   return (
     <Text>
       Error screen to be built. Error message: {route.params.errorMessage}
@@ -219,23 +368,26 @@ const LoadingIndicator = () => {
 
 //#region Web Screens
 const DivbloxWebAppWrapperScreen = ({navigation, route}) => {
-  React.useEffect(
-    () =>
-      navigation.addListener('beforeRemove', e => {
-        // Prevent default behavior of leaving the screen
-        e.preventDefault();
-        console.log('Here we must send the back signal to the web app');
-      }),
-    [navigation],
-  );
+  React.useEffect(() => {
+    DivbloxReactNative.setNavigation(navigation, 'WebWrapper');
+    navigation.addListener('beforeRemove', e => {
+      // Prevent default behavior of leaving the screen
+      e.preventDefault();
+      console.log('Here we must send the back signal to the web app');
+    });
+    (async () => {
+      await AsyncStorage.setItem('isFirstLaunch', '1');
+    })();
+  }, [navigation]);
   return (
     <>
       <SafeAreaView style={styles.webContainerTop} />
       <SafeAreaView style={styles.webContainerBottom}>
         <StatusBar barStyle={statusBarStyle} />
         <WebView
+          startInLoadingState={true}
           style={{width: dimensions.width}}
-          source={{uri: serverBaseUrl}}
+          source={{uri: DivbloxReactNative.getWebWrapperUrl()}}
           renderLoading={() => {
             return LoadingIndicator();
           }}
@@ -253,7 +405,16 @@ const DivbloxWebAppWrapperScreen = ({navigation, route}) => {
 
 //#region Pure Native Screens
 const DivbloxPureNativeWelcomeScreen = ({navigation, route}) => {
+  React.useEffect(() => {
+    DivbloxReactNative.setNavigation(navigation, 'Welcome');
+  }, [navigation]);
   return <Text>Welcome screen to be built</Text>;
+};
+const DivbloxPureNativePlaceholderScreen = ({navigation, route}) => {
+  React.useEffect(() => {
+    DivbloxReactNative.setNavigation(navigation, 'Placeholder');
+  }, [navigation]);
+  return <Text>Placeholder screen to be built</Text>;
 };
 //#endregion
 
@@ -273,6 +434,8 @@ const DivbloxWebStack = () => {
             name="WebWrapper"
             component={DivbloxWebAppWrapperScreen}
           />
+          <Stack.Screen name="Offline" component={DivbloxGlobalOfflineScreen} />
+          <Stack.Screen name="Error" component={DivbloxGlobalErrorScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
@@ -288,6 +451,12 @@ const DivbloxPureNativeStack = () => {
           options={{title: 'Init'}}
         />
         <Stack.Screen name="Welcome" component={DivbloxGlobalWelcomeScreen} />
+        <Stack.Screen
+          name="Placeholder"
+          component={DivbloxPureNativePlaceholderScreen}
+        />
+        <Stack.Screen name="Offline" component={DivbloxGlobalOfflineScreen} />
+        <Stack.Screen name="Error" component={DivbloxGlobalErrorScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
